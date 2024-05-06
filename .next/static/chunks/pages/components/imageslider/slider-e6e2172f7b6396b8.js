4b99530818f38e64e3dc21add1abccd068192eb5
Vijaypandhare <168817318+Vijaypandhare@users.noreply.github.com>
2024-05-06 03:01:32 -0700

    Your commit message

--- .next/static/chunks/pages/components/imageslider/slider-e6e2172f7b6396b8.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[868],{1999:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/imageslider/slider",function(){return u(7449)}])},7449:function(){}},function(n){n.O(0,[888,774,179],function(){return n(n.s=1999)}),_N_E=n.O()}]);
